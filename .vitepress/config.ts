a79f300753ed7c36a758414689aec8c7ce3bf385
Soybean <soybeanjs@outlook.com>
2025-12-25 19:31:28 +0800

    feat(projects): update favicon link to use new logo.svg

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
         content: 'width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no'
       }
     ],
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ['link', { rel: 'icon', href: '/logo.svg' }]
   ],
   assetsDir: 'public',
   srcDir: 'src',
